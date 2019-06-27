dc47ab41fba392298f3f87a2f5baa34c25441cb2
kerodeo <kerodeo@gmail.com>
2019-06-27 14:34:07 +0900

    尝试

--- sw.js
+++ sw.js
@@ -1,88 +1,52 @@
-var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var APP_PREFIX = 'pwatest_'
+var VERSION = 'version_01' // v.3 includes contribution from @mathias expanding event target to entire page
 var CACHE_NAME = APP_PREFIX + VERSION
-var urlsToCache = [                            // Add URL you want to cache in this list.
-    '/pwatest/',                        // If you have separate JS/CSS files,
-    '/pwatest/index.html'               // add path to those files here
+var URLS = [
+    '/pwatest/',
+    '/pwatest/index.html'
 ]
-
-// Cache resources
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        // Perform install steps
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(urlsToCache)
-        })
-    )
-})
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
-
     e.respondWith(
-        caches.match(e.request).then(function (response) {
-            // if cache is available, respond with cache
-            if (response) { 
-                console.log('responding with cache : ' + e.response.url)
-                return response
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
             }
 
-            console.log('file is not cached, fetching : ' + e.response.url)
-
-            // IMPORTANT:Clone the request. A request is a stream and
-            // can only be consumed once. Since we are consuming this
-            // once by cache and once by the browser for fetch, we need
-            // to clone the response.
-            var fetchRequest = event.request.clone();
-            return fetch(fetchRequest).then(
-                function (response) {
-                    // Check if we received a valid response
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response;
-                    }
-
-                    // IMPORTANT:Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
-                    var responseToCache = response.clone();
-                    
-                    caches.open(CACHE_NAME)
-                        .then(function (cache) {
-                            cache.put(event.request, responseToCache);
-                        });
-                    
-                    return response;
-                }
-            );
+            // You can omit if/else for console.log & put one line below here too.
+            // return request || fetch(e.request)
         })
-    );         
-});
+    )
+})
 
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(URLS)
+        })
+    )
+})
 
-// Delete outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            // `cacheNames` contains all cache names under your username.github.io
-            // filter out ones that has this app prefix to create white list
-            var cacheWhitelist = cacheNames.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-
-            // add current cache name to white list
-            cacheWhitelist.push(CACHE_NAME);
-
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        console.log('deleting cache : ' + cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            )
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME)
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i])
+                    return caches.delete(keyList[i])
+                }
+            }))
         })
     )
 })
